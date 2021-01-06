8ff5303fe5c739e0d454832356700b47ad5b3c53
wdong20 <wdong20@stuy.edu>
2021-01-06 12:15:22 -0500

    Created skeletons for required methods

---
+++ MyLinkedList.java
@@ -0,0 +1,32 @@
+public class MyLinkedList {
+    private int size;
+    private Node start, end;
+
+    public MyLinkedList() {
+
+    }
+
+    public int size() {
+        return -1;
+    }
+
+    public boolean add(String value) {
+        return false;
+    }
+
+    public boolean add(int index, String value) {
+        return false;
+    }
+    
+    public String get(int index) {
+        return null;
+    }
+
+    public String set(int index, String value) {
+        return null;
+    }
+
+    public String toString() {
+        return null;
+    }
+}
