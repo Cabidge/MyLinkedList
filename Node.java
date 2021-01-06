60fc6b48487b91bbda2f25a11a063acc68a5edf6
wdong20 <wdong20@stuy.edu>
2021-01-06 12:07:18 -0500

    Created skeletons for all the methods

---
+++ Node.java
@@ -0,0 +1,34 @@
+import javax.xml.crypto.Data;
+
+public class Node {
+    private String data;
+    private Node next, prev;
+
+    public Node(String value) {
+
+    }
+
+    public void setData(String value) {
+
+    }
+
+    public String getData() {
+
+    }
+
+    public void setNext(Node node) {
+
+    }
+
+    public Node getNext() {
+
+    }
+
+    public void setPrev(Node node) {
+
+    }
+
+    public Node getPrev() {
+
+    }
+}
